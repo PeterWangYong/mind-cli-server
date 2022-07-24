e7cc3474d30ee98025d63c8bb050a141f4c3767a
PeterWangYong <763874832@qq.com>
2022-07-24 19:28:12 +0800

    feat: first submit

---
+++ config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+// Mongodb
+const mongodbUrl = "mongodb://root:password@www.peterwangyong.xyz:27017";
+const mongodbDbName = "mind-cli";
+module.exports = {
+  mongodbUrl,
+  mongodbDbName,
+};
